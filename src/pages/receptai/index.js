68c283707fd1fed3bf123f33c2770b8242c0659a
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-16 13:56:43 +0100

    getStaticProps

--- src/pages/receptai/index.js
+++ src/pages/receptai/index.js
@@ -10,24 +10,25 @@ const inter = Inter({ subsets: ['latin'] });
 
 const url = 'https://dummyjson.com/recipes';
 
-export default function Receptai() {
-  const [receptArr, setReceptArr] = useState([]);
+export default function Receptai({ receptai }) {
+  const [receptArr, setReceptArr] = useState(receptai);
+  console.log('receptai ===', receptai);
   // parsiusti receptus is dummy JSON
-  useEffect(() => {
-    getReceptus();
-  }, []);
+  // useEffect(() => {
+  //   getReceptus();
+  // }, []);
 
-  const getReceptus = () => {
-    axios
-      .get(url)
-      .then((resp) => {
-        const resceptai = resp.data.recipes;
-        setReceptArr(resceptai);
-      })
-      .catch((error) => {
-        console.warn('ivyko klaida:', error);
-      });
-  };
+  // const getReceptus = () => {
+  //   axios
+  //     .get(url)
+  //     .then((resp) => {
+  //       const resceptai = resp.data.recipes;
+  //       setReceptArr(resceptai);
+  //     })
+  //     .catch((error) => {
+  //       console.warn('ivyko klaida:', error);
+  //     });
+  // };
   // sugeneruoti saraso ar korteliu pavidalu
   return (
     <>
@@ -45,4 +46,34 @@ export default function Receptai() {
       </div>
     </>
   );
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  function getData() {
+    return fetch(url)
+      .then((resp) => resp.json())
+      .then((atsObj) => {
+        console.log('atsObj ===', atsObj);
+        return atsObj.recipes;
+      })
+      .catch((error) => {
+        console.warn('ivyko klaida:', error);
+      });
+  }
+
+  const receptai = await getData();
+  console.log(
+    'receptai ===',
+    receptai.map(({ name }) => name)
+  );
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      receptai,
+    },
+  };
 }
